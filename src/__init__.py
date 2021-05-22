b75c66bbb7b0ce92f6884f376b5f63264c47c467
Michael Herman <hermanmu@gmail.com>
2021-05-22 14:44:04 -0500

    update swagger

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,7 @@ import os
 from flask import Flask
 from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 # instantiate the extensions
 db = SQLAlchemy()
@@ -16,6 +17,7 @@ def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
     # set config
     app_settings = os.getenv("APP_SETTINGS")
@@ -26,13 +28,10 @@ def create_app(script_info=None):
     if os.getenv("FLASK_ENV") == "development":
         admin.init_app(app)
 
-    # register blueprints
-    from src.api.ping import ping_blueprint
+    # register api
+    from src.api import api
 
-    app.register_blueprint(ping_blueprint)
-    from src.api.users.views import users_blueprint
-
-    app.register_blueprint(users_blueprint)
+    api.init_app(app)
 
     # shell context for flask cli
     @app.shell_context_processor
